22a094d6e1f31e759e22738327bc8456d9ed8592
Mariosam2 <marco.mariosaa@gmail.com>
2022-11-09 14:10:01 +0100

    added jumbotron component

--- bonus/vite-hello-world/src/App.vue
+++ bonus/vite-hello-world/src/App.vue
@@ -1,11 +1,21 @@
 <script>
-export default {
+import Jumbotron from './components/Jumbotron.vue';
 
+export default {
+  components: {
+    Jumbotron,
+  }
 }
 </script>
 <template>
+
+  <Jumbotron></Jumbotron>
   <h1>Ciao Vite</h1>
+
 </template>
 <style>
-
+img {
+  max-width: 100%;
+  height: auto;
+}
 </style>
---
+++ bonus/vite-hello-world/src/components/Jumbotron.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+
+}
+</script>
+<template>
+    <section class="jumbo">
+        <img src="../assets/bg.jpg" alt="jumbo-img">
+    </section>
+</template>
+<style>
+.jumbo {
+    height: 700px;
+}
+
+.jumbo img {
+    height: 100%;
+    object-fit: cover;
+}
+</style>
